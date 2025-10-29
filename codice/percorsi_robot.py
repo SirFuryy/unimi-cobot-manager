1699bcd0526e2092c2a52059daf783c47473da9c
giacomo <giacomotommi@gmail.com>
2025-10-29 15:47:27 +0100

    Reso classe camera_handler

--- codice/percorsi_robot.py
+++ codice/percorsi_robot.py
@@ -5,17 +5,21 @@ import threading
 from typing import Tuple, List
 
 from pose_class import Pose
-import camera_handler
+from camera_handler import CameraHandler
 import robot_controller
 from multi_terminal_gui import MultiTerminalGUI
+
+global zed
+zed: CameraHandler = CameraHandler()
 
 
 def start_scanning(pose: Pose, gui: MultiTerminalGUI, plant_name: str, frames_to_record: int = 300):
     """Avvia la scansione in background."""
-
-    camera_handler.record_cam(pose, gui, plant_name, frames_to_record)
-
-    gui.write_to_terminal(2, f"Percorsi - Scansione avviata per {plant_name} con {frames_to_record} fotogrammi.")
+    global zed
+    try:
+        zed.record_cam(pose, gui, plant_name, frames_to_record)
+    except Exception as e:
+        gui.write_to_terminal(4, f"Errore durante la registrazione: {e}")
 
 def scan_plant(bbox, plant_name: str, dashboard, move, gui: MultiTerminalGUI, frames_to_record: int = 300):
     """
@@ -81,7 +85,7 @@ def movement_first_quadrant(bbox, plant_name: str, dashboard, move, gui: MultiTe
     
     # Avvia la scansione in background
     pose = Pose.crea_pose_from_coord(robot_controller.get_current_pose(dashboard))
-    #threading.Thread(target=start_scanning, args=(pose, gui, plant_name, frames_to_record), daemon=True).start()
+    threading.Thread(target=start_scanning, args=(pose, gui, plant_name, frames_to_record), daemon=True).start()
     
     # reach the top vision of the plant
     gui.write_to_terminal(1, "Pronto per raggiungere top.")
@@ -123,12 +127,6 @@ def movement_first_quadrant(bbox, plant_name: str, dashboard, move, gui: MultiTe
     
     # return to ambient high vision point
     robot_controller.RunPoint(dashboard, move, gui, start_joints)
-    
-    # TODO: calcolare i 5 punti di scansione. Il primo lo si calcola prendendo il centro della piantina, ponendo z massimo per la piantina e aggiungendo 35 cm per stare larhi. Poi farlo in diagonale di 60 gradi, prendendo z massimi e x e y rispettivamente massimi e minimi per i quattro altri punti
-    
-    # TODO: fare runpoint su quei punti nel mentre che la scannsione è già aprtita, bisogna capire quanto tempo ci mette la scansione e regolare i tempi
-    
-    # TODO: modificare le funzioni di robot controller per renderle più efficaci e togliere quella cagata di get angle se non è raggiungibile    
    
     
 def movement_second_quadrant(bbox, plant_name: str, dashboard, move, gui: MultiTerminalGUI, frames_to_record: int = 300):
@@ -143,7 +141,7 @@ def movement_second_quadrant(bbox, plant_name: str, dashboard, move, gui: MultiT
     print("Percorsi - Movimento nel secondo quadrante.")
     center_z_max = [bbox[0], bbox[1], bbox[2]+bbox[5]/2]   #Prendo z max
 
-    coord_top_vision_plant = [center_z_max[0], center_z_max[1], center_z_max[2]+350.0, -180.0000, 0.0000, 90.0000]
+    coord_top_vision_plant = [center_z_max[0], center_z_max[1], center_z_max[2]+350.0, -180.0000, 0.0000, 180.0000]
 
     # arriva al punto iniziale di scansione generale
     start_joints = [103.0000, 39.0000, -86.0000, -24.0000, 88.0000, 195.0000]
